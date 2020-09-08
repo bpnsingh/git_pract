cf1884b3ea58bf877652a6c6b5cc5f6ea491c5ad
bipinsengar <bsengar@dialpad.com>
2020-09-08 07:12:53 +0530

    linear data strucuture

---
+++ codeAcademy/Data_Structure/Linked_list_python.py
@@ -0,0 +1,58 @@
+class Node:
+    def __init__(self,value,next_node=None):
+        self.value=value
+        self.next_node=next_node
+    def get_value(self):
+        return self.value
+    def get_next_node(self):
+        return self.next_node
+    def set_next_node(self,next_node):
+        self.next_node = next_node
+
+class LinkedList:
+    def __init__(self,value=None):
+        self.head_node = Node(value)
+    def get_head_node(self):
+        return self.head_node
+    def insert_beginning(self,new_value):
+        new_node=Node(new_value)
+        new_node.set_next_node(self.head_node)
+        self.head_node = new_node
+    def stringify_list(self):
+        srting_list = ""
+        current_node=self.get_head_node()
+        while current_node:
+            if current_node.get_value() != None:
+                srting_list += str(current_node.get_value()) + "\n"
+            current_node = current_node.get_next_node()
+        return srting_list
+    def remove_node(self,value_to_remove):
+        current_node=self.get_head_node()
+        if value_to_remove == current_node.get_value():
+            self.head_node = current_node.next_node
+        else:
+            while current_node:
+                next_node = current_node.get_next_node()
+                if next_node.get_value() == value_to_remove:
+                    current_node.set_next_node(next_node.get_next_node())
+                    current_node = None
+                else:
+                    current_node = next_node
+
+
+
+
+
+
+
+
+
+
+#my_node=Node(44)
+#print (my_node.value)
+
+ll = LinkedList(5)
+ll.insert_beginning(70)
+ll.insert_beginning(5675)
+ll.insert_beginning(90)
+print(ll.stringify_list())
